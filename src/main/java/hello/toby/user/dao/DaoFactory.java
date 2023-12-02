ee17ff0403e39fd1e3a2b21e1c9fc69d90abc0bb
SangCheol <scpark0698@gmail.com>
2023-12-03 02:06:59 +0900

    feat: daoV6 DataSource 인터페이스 이용

--- src/main/java/hello/toby/user/dao/DaoFactory.java
+++ src/main/java/hello/toby/user/dao/DaoFactory.java
@@ -2,18 +2,31 @@ package hello.toby.user.dao;
 
 import hello.toby.user.dao.daoV5.ConnectionMaker;
 import hello.toby.user.dao.daoV5.DConnectionMaker;
-import hello.toby.user.dao.daoV5.UserDaoV5;
+import hello.toby.user.dao.daoV6.UserDaoV6;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 public class DaoFactory {
 
     @Bean
     public UserDao userDao() {
-        return new UserDaoV5(connectionMaker());
+        //return new UserDaoV5(connectionMaker());
+        return new UserDaoV6(dataSource());
     }
 
     @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:h2:tcp://localhost/~/toby");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
     }
 }
---
+++ src/main/java/hello/toby/user/dao/daoV6/UserDaoV6.java
@@ -0,0 +1,62 @@
+package hello.toby.user.dao.daoV6;
+
+import hello.toby.user.dao.UserDao;
+import hello.toby.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * DataSource 인터페이스 이용
+ */
+public class UserDaoV6 implements UserDao {
+
+    private final DataSource dataSource;
+
+    public UserDaoV6(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void add(User user) throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values (?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    @Override
+    public User get(String id) throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
