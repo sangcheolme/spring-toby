c353ea97305cc668949a928560920eececd24544
SangCheol <scpark0698@gmail.com>
2024-01-18 19:30:50 +0900

    feat: UserDaoTestV2 getUserFailure() 테스트 추가

--- src/main/java/hello/toby/user/dao/daoV6/UserDaoV6.java
+++ src/main/java/hello/toby/user/dao/daoV6/UserDaoV6.java
@@ -2,6 +2,7 @@ package hello.toby.user.dao.daoV6;
 
 import hello.toby.user.dao.UserDao;
 import hello.toby.user.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -47,15 +48,22 @@ public class UserDaoV6 implements UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         c.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
 
         return user;
     }
--- src/test/java/hello/toby/UserDaoTestV2.java
+++ src/test/java/hello/toby/UserDaoTestV2.java
@@ -6,10 +6,12 @@ import hello.toby.user.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * UserDao에 deleteAll()와 getCount() 기능을 추가하여 반복 가능한 테스트 만들기
@@ -18,24 +20,26 @@ public class UserDaoTestV2 {
 
     @Test
     void addAndGet() throws SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDaoV6 userDao = context.getBean("userDaoV6", UserDaoV6.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDaoV6 userDao = ac.getBean("userDaoV6", UserDaoV6.class);
+
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
 
         userDao.deleteAll();
         assertThat(userDao.getCount()).isEqualTo(0);
 
-        User user = new User();
-        user.setId("testId");
-        user.setName("testName");
-        user.setPassword("testPw");
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
 
-        userDao.add(user);
-        assertThat(userDao.getCount()).isEqualTo(1);
+        User findUser1 = userDao.get(user1.getId());
+        assertThat(user1.getName()).isEqualTo(findUser1.getName());
+        assertThat(user1.getPassword()).isEqualTo(findUser1.getPassword());
 
-        User findUser = userDao.get(user.getId());
-
-        assertThat(user.getName()).isEqualTo(findUser.getName());
-        assertThat(user.getPassword()).isEqualTo(findUser.getPassword());
+        User findUser2 = userDao.get(user2.getId());
+        assertThat(user2.getName()).isEqualTo(findUser2.getName());
+        assertThat(user2.getPassword()).isEqualTo(findUser2.getPassword());
     }
 
     @Test
@@ -58,5 +62,18 @@ public class UserDaoTestV2 {
 
         userDao.add(user3);
         assertThat(userDao.getCount()).isEqualTo(3);
+    }
+
+    @Test
+    void getUserFailure() throws SQLException {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDaoV6 userDao = ac.getBean("userDaoV6", UserDaoV6.class);
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+
+        assertThatThrownBy(() -> userDao.get("unknown_id"))
+                .isInstanceOf(EmptyResultDataAccessException.class);
+
     }
 }
